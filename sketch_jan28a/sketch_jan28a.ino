56956a86bca386283da8375187b3a8f420a54eaf
shay gladden <saglad1@ilstu.edu>
2025-01-28 15:03:17 -0600

    Lab4(Display)

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -4,13 +4,23 @@
 
 U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
+DHT dht(3, DHT11);
+
 void setup() {
 u8x8.begin();
 u8x8.setFlipMode(1);
 u8x8.clear();
+dht.begin();
 }
 void loop() {
 u8x8.setFont(u8x8_font_chroma48medium8_r);
 u8x8.setCursor(0, 0);
-u8x8.print("Hello World!");
+u8x8.print("Hum: ");
+u8x8.println(dht.readHumidity());
+u8x8.println("%");
+u8x8.print("Temp: ");
+u8x8.println(dht.readTemperature());
+u8x8.println("C");
+float temp = dht.readTemperature();
+float humidity = dht.readHumidity();
 }
